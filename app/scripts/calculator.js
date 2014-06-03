3220e47cce49283a7a26e7777d8f9188e8dea16e
Marcus Erlandsson <mail@marquz.com>
2014-06-03 12:26:36 +0200

    Added initial calculator tests

---
+++ app/scripts/calculator.js
@@ -0,0 +1,9 @@
+var Calculator = (
+  function() {
+    return {
+      add: function(number1, number2) {
+        return number1 + number2;
+      }
+    };
+  }
+)();
--- test/spec/test.js
+++ test/spec/test.js
@@ -1,21 +1,13 @@
 /* global describe, it */
+require('calculator.js');
 
 (function () {
     'use strict';
 
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
-
-            });
-        });
-    });
-
     describe('Calculator', function() {
-        describe('Adding numbers', function () {
+        describe('Adding two numbers', function () {
             it('should return sum of numbers', function() {
-                var foo = function(a, b) { return a+b};
-                foo(3,5).should.equal(8);
+                Calculator.add(1, 2).should.equal(3);
             });
         });
     });
