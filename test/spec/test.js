d7afa101ab5b4afc62a9591be308812dd2e4fae6
Arek Mytych <amytych@gmail.com>
2014-06-04 15:42:45 +0200

    Added more test cases

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,26 +1,31 @@
 /* global describe, it */
 var Calculator = (
   function() {
+    'use strict';
+
     return {
       add: function (numberString) {
         var numberParts,
           numberPartsInt = [],
           sum = 0,
-          separators = '\n,',
+          separators = '\\n,',
           separatorsRegExp = '';
 
-        if (numberString === "") return 0;
+        if (numberString === '') {
+          return 0;
+        }
+
         var lengthOfPrefix = 2;
-        if (numberString.substr(lengthOfPrefix) === "//") {
+        if (numberString.substr(0, lengthOfPrefix) === '//') {
           var indexOfLineBreak = numberString.indexOf('\n');
           var separator = numberString.substring(lengthOfPrefix, indexOfLineBreak);
           separators += separator;
         }
 
-
-
-        separatorsRegExp = new RegExp(separators, 'g');
+        separatorsRegExp = new RegExp('[' + separators + ']', 'g');
+        console.log(separatorsRegExp);
         numberParts = numberString.split(separatorsRegExp);
+        console.log(numberParts);
         for (var i = numberParts.length - 1; i >= 0; i--) {
           numberPartsInt[i] = parseInt(numberParts[i], 10);
           sum += numberPartsInt[i];
@@ -32,26 +37,26 @@ var Calculator = (
 )();
 
 (function () {
-    'use strict';
+  'use strict';
 
-    describe('Calculator', function() {
-        it('should return 0 if empty string', function() {
-          Calculator.add("").should.equal(0);
-        });
-        it('should return number if given a single number', function() {
-          Calculator.add("4").should.equal(4);
-        });
-        it('should return sum if two numbers', function() {
-          Calculator.add("4,3").should.equal(7);
-        });
-        it('should return sum if four numbers', function() {
-          Calculator.add("4,3,2,1").should.equal(10);
-        });
-        it('should handle new line and comma as separator', function() {
-          Calculator.add("1\n2,3").should.equal(6);
-        });
-        it('should handle custom separators', function() {
-          Calculator.add("//;\n1\n2,3;10").should.equal(16);
-        });
+  describe('Calculator', function() {
+    it('should return 0 if empty string', function() {
+      Calculator.add('').should.equal(0);
     });
+    it('should return number if given a single number', function() {
+      Calculator.add('4').should.equal(4);
+    });
+    it('should return sum if two numbers', function() {
+      Calculator.add('4,3').should.equal(7);
+    });
+    it('should return sum if four numbers', function() {
+      Calculator.add('4,3,2,1').should.equal(10);
+    });
+    it('should handle new line and comma as separator', function() {
+      Calculator.add('1\n2,3').should.equal(6);
+    });
+    it('should handle custom separators', function() {
+      Calculator.add('//;\n1\n2,3;10').should.equal(16);
+    });
+  });
 })();
